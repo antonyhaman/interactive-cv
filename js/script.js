d74091e409ee28354932708c8e40ee2ddc9949e4
kotvertolet <bytesurfer5005@gmail.com>
2017-06-12 18:17:01 +0300

    Reverting some changes

--- js/script.js
+++ js/script.js
@@ -22,43 +22,43 @@ $(function() {
     });
 });
 
-$(function() {
-    $(document).ready(function() {
-        $.getJSON("json/getCareersData", function(response) {
-            renderCareers(response);
-        })
-    });
-})
+// $(function() {
+//     $(document).ready(function() {
+//         $.getJSON("json/getCareersData", function(response) {
+//             renderCareers(response);
+//         })
+//     });
+// })
 
-function renderCareers(response) {
-    $(document).ready(function() {
-        let jobs = [];
-        response.forEach(function(respElem) {
-            let divWork = $(htmlElements.workplaceDiv);
-            let divDetails = $(htmlElements.jobDetailsDiv);
+// function renderCareers(response) {
+//     $(document).ready(function() {
+//         let jobs = [];
+//         response.forEach(function(respElem) {
+//             let divWork = $(htmlElements.workplaceDiv);
+//             let divDetails = $(htmlElements.jobDetailsDiv);
 
-            $(htmlElements.strong).text(respElem.workplace).appendTo(divWork);
-            $(htmlElements.paragraph).text(respElem.date).appendTo(divWork);
-            $(htmlElements.strong).text(respElem.position).appendTo(divDetails);
-            $(htmlElements.paragraph).text(respElem.description).appendTo(divDetails);
+//             $(htmlElements.strong).text(respElem.workplace).appendTo(divWork);
+//             $(htmlElements.paragraph).text(respElem.date).appendTo(divWork);
+//             $(htmlElements.strong).text(respElem.position).appendTo(divDetails);
+//             $(htmlElements.paragraph).text(respElem.description).appendTo(divDetails);
 
 
-            if (respElem.subtopic != undefined) {
-     			let responsibilitiesList = $(htmlElements.responsibilitiesList);
-                $(htmlElements.paragraph).text(respElem.subtopic).appendTo(divDetails);
-                respElem.responsibilities.forEach(function(elem) {
+//             if (respElem.subtopic != undefined) {
+//      			let responsibilitiesList = $(htmlElements.responsibilitiesList);
+//                 $(htmlElements.paragraph).text(respElem.subtopic).appendTo(divDetails);
+//                 respElem.responsibilities.forEach(function(elem) {
 
-                    $(htmlElements.responsibilitiesListElement).text(elem).appendTo(responsibilitiesList);
-                })
-                divDetails.append(responsibilitiesList);
-            }
+//                     $(htmlElements.responsibilitiesListElement).text(elem).appendTo(responsibilitiesList);
+//                 })
+//                 divDetails.append(responsibilitiesList);
+//             }
 
-            jobs.push(divWork);
-            jobs.push(divDetails);
-        })
+//             jobs.push(divWork);
+//             jobs.push(divDetails);
+//         })
 
-        jobs.forEach(function(elem) {
-            $('#careers .row').append(elem);
-        })
-    });
-}
+//         jobs.forEach(function(elem) {
+//             $('#careers .row').append(elem);
+//         })
+//     });
+// }
